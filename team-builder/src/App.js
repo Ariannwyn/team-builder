bf370d2e81c46bbc3994d5e394c60a95d6b2e7f2
EmilyA <emilyadams.3d@gmail.com>
2020-08-13 09:43:31 -0700

    Adding FormResults file

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Form from './components/Form';
 import './App.css';
+import FormResults from './components/FormResults';
 
 function App() {
 const [teamMember, setTeamMember] = useState([{
@@ -20,13 +21,11 @@ const addNewMember = member => {
   setTeamMember([...teamMember, newMember])
 }
 
-
-
   return (
     <div className="App">
       <h1>Team Builder</h1>
       <Form addNewMember={addNewMember}/>
-      <p>{teamMember.name}</p>
+      <FormResults teamMember={teamMember}/>
     </div>
   );
 }
---
+++ team-builder/src/components/FormResults.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FormResults = props => {
+    return(
+       props.teamMember.map(member => {
+                <div className="member" key={member.id}>
+                    <p>{member.name}</p>
+                    <p>{member.email}</p>
+                    <p>{member.role}</p>
+                </div>
+            })
+       
+    )
+}
+
+export default FormResults;
